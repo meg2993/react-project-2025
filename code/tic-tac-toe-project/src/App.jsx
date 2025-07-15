bcaefb04913914dc0b484f7339f2417746c558dc
Meghana Nagendra <nmeghana003@gmail.com>
2025-07-15 14:49:17 -0400

    code for tic-tac-toe game

---
+++ code/tic-tac-toe-project/src/App.jsx
@@ -0,0 +1,21 @@
+import Player from "./components/Player";
+
+function App() {
+  
+
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player name="Player 1" symbol="X" />
+                    <Player name="Player 2" symbol="O" />
+                </ol>
+
+                PLACEHOLDER FOR GAME COMPONENT
+            </div>
+            LOG
+        </main>
+    );
+}
+
+export default App
